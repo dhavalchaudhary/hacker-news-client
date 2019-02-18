122c1bff31f557667de52e1edf77eb3925055ab9
dhavalchaudhary <dhavalchaudhary96@gmail.com>
2019-02-18 21:03:10 +0800

    hide lode more button when fetching

--- src/containers/App/index.ts
+++ src/containers/App/index.ts
@@ -16,8 +16,19 @@ const mapStateToProps = (state: AppStateType) => {
     return sum;
   }, 0);
   const totalStoriesDisplayed = state.user.ui.pages.length * STORIES_PER_PAGE;
+  const allPages = state.user.ui.pages;
+  let isLoading = false;
+  if (allPages.length > 0) {
+    const lastPageIds = state.user.ui.pages[state.user.ui.pages.length - 1];
+    isLoading =
+      lastPageIds.filter(
+        i =>
+          state.stories.hasOwnProperty(i.storyId) &&
+          state.stories[i.storyId]!.fetchStatus === "loading"
+      ).length > 0;
+  }
   return {
-    loadMoreValid: totalStories - totalStoriesDisplayed > 0,
+    loadMoreValid: totalStories - totalStoriesDisplayed > 0 && !isLoading,
     theme: state.user.ui.theme
   };
 };
